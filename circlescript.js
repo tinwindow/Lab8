c434452753439458b50dd80da1a287924a65eddf
tinwindow <erin.ura@gmail.com>
2017-07-24 08:56:48 -0400

    Lab 8 First Try

---
+++ circlescript.js
@@ -0,0 +1,44 @@
+		
+		
+		var area; 
+
+		function areaOfCircle() { 
+      	 radius = document.getElementById("userinput").value; 
+      	 area = ((radius * radius) * Math.PI); 
+      	 console.log(area);
+      	 
+    		
+    	}
+
+    	function showArea(){
+  		var userInput = document.getElementById("userinput").value;
+  		document.getElementById("userMsg").innerHTML = "The area of a circle with a radius of " + radius + " is " + area;
+}
+    	
+    	
+
+
+    	//function circleSize(areaOfCircle) {
+    		//var circle = document.getElementById("circle");
+    		//if area <= 50; 
+    		
+    		
+		//}
+
+
+    	
+
+      	 
+      		
+
+
+
+    	
+
+
+    	
+    //must apply console.log statement inside function with a parameter (radius); outside function can just call.	
+
+    // var radius - document.getElementsById('userinput')
+
+    //var radius = document.calculateradius.userInput.value;
